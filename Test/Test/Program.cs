f736e1920acdbf8b74ce0aa24865f7e8a1893b81
unknown <stefanogabriela1@gmail.com>
2025-06-10 11:00:52 +0300

    Test

---
+++ Test/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = {4, 3, 2, 36, 6, 12, 11, 23, 5, 8, 1, 7};
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int minNumber = i;
+
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        if (numbers[minNumber] < numbers[j])
+        {
+            minNumber = j;
+        }
+
+        int c = numbers[i];
+        numbers[i] = numbers[minNumber];
+        numbers[minNumber] = c;
+    }
+}
+Console.Write("Подреден масив: ");
+Console.WriteLine(string.Join(" ", numbers));
